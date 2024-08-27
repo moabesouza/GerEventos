8fdaadd8b6dfebc08f599f834a175c8942984484
moabesouza <moabesouza6@gmail.com>
2024-08-27 17:01:01 -0300

    refatoração de validação para decimal

---
+++ GerEventos/Attributes/DecimalAttribute.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace GerEventos.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class DecimalAttribute : ValidationAttribute, IClientModelValidator
+    {
+        public int MaxLength { get; set; }
+
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            MergeAttribute(context.Attributes, "data-decimal", "true");
+
+            if (MaxLength > 0)
+            {
+                MergeAttribute(context.Attributes, "maxlength", MaxLength.ToString());
+            }
+        }
+
+        private bool MergeAttribute(IDictionary<string, string> attributes, string key, string value)
+        {
+            if (attributes.ContainsKey(key))
+            {
+                return false;
+            }
+
+            attributes.Add(key, value);
+            return true;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                // Converte o valor para string
+                var stringValue = value.ToString();
+
+                // Substitui a vírgula pelo ponto
+                stringValue = stringValue.Replace(',', '.');
+
+                // Tenta converter a string para decimal
+                if (decimal.TryParse(stringValue, out decimal decimalValue))
+                {
+                  
+                    return ValidationResult.Success;
+                }
+                else
+                {
+              
+                    return new ValidationResult(ErrorMessage ?? "O valor deve ser um número decimal válido.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
--- GerEventos/Pages/Produtor/index.js
+++ GerEventos/Pages/Produtor/index.js
@@ -14,7 +14,7 @@ $(function () {
             ajax: abp.libs.datatables.createAjax(gerEventos.services.produtores.produtor.getList),
             columnDefs: [
                 {
-                    title: l('Grid:Acoes"'),
+                    title: l('Grid:Acoes'),
                     rowAction: {
                         items: [
                             {
